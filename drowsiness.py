ef1acea6aef16ad6de560b3047d896f8f69b6cd2
sbanerjee29 <104590401+sbanerjee29@users.noreply.github.com>
2024-01-27 21:53:55 +0530

    Update drowsiness.py

--- drowsiness.py
+++ drowsiness.py
@@ -1,233 +1,166 @@
-import dlib
-import sys
-import cv2
-import time
-import numpy as np
+
 from scipy.spatial import distance as dist
+from imutils.video import VideoStream
+from imutils import face_utils
 from threading import Thread
-from pydub import AudioSegment
-from pydub.playback import play
-import queue
+import numpy as np
+import argparse
+import imutils
+import time
+import dlib
+import cv2
+import os
 
-# Constants
-FACE_DOWNSAMPLE_RATIO = 1.5
-RESIZE_HEIGHT = 460
-thresh = 0.27
-modelPath = r"/Users/shrestabanerjee/Downloads/shape_predictor_70_face_landmarks.dat"
-sound_path = "/Users/shrestabanerjee/Music/Music/Media.localized/Music/Unknown Artist/Unknown Album/y2mate.com - Wake up SidClub mix Full  Title TrackRanbir KapoorShankar MahadevanJaved Akhtar.mp3"
+def alarm(msg):
+    global alarm_status
+    global alarm_status2
+    global saying
 
-# Initialize dlib
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor(modelPath)
+    while alarm_status:
+        print('call')
+        s = 'espeak "'+msg+'"'
+        os.system(s)
 
-# Define eye landmarks
-leftEyeIndex = [36, 37, 38, 39, 40, 41]
-rightEyeIndex = [42, 43, 44, 45, 46, 47]
+    if alarm_status2:
+        print('call')
+        saying = True
+        s = 'espeak "' + msg + '"'
+        os.system(s)
+        saying = False
 
-# Global variables
-blinkCount = 0
-drowsy = 0
-state = 0
-blinkTime = 0.15  # 150ms
-drowsyTime = 1.5  # 1200ms
-ALARM_ON = False
-GAMMA = 1.5
-threadStatusQ = queue.Queue()
-
-# Gamma correction table
-invGamma = 1.0 / GAMMA
-table = np.array([((i / 255.0) ** invGamma) * 255 for i in range(0, 256)]).astype("uint8")
-
-# Function for gamma correction
-def gamma_correction(image):
-    return cv2.LUT(image, table)
-
-# Function for histogram equalization
-def histogram_equalization(image):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    return cv2.equalizeHist(gray)
-
-# Function to play sound alert
-def soundAlert(path, threadStatusQ):
-    try:
-        alert_sound = AudioSegment.from_wav(path)
-        while True:
-            if not threadStatusQ.empty():
-                FINISHED = threadStatusQ.get()
-                if FINISHED:
-                    break
-            play(alert_sound)
-    except Exception as e:
-        print("Error while playing sound:", str(e))
-
-# Function to calculate eye aspect ratio
 def eye_aspect_ratio(eye):
     A = dist.euclidean(eye[1], eye[5])
     B = dist.euclidean(eye[2], eye[4])
+
     C = dist.euclidean(eye[0], eye[3])
+
     ear = (A + B) / (2.0 * C)
+
     return ear
 
-# Function to check eye status (open or closed)
-def checkEyeStatus(landmarks):
-    mask = np.zeros(frame.shape[:2], dtype=np.float32)
-    
-    hullLeftEye = []
-    for i in range(0, len(leftEyeIndex)):
-        hullLeftEye.append((landmarks[leftEyeIndex[i]][0], landmarks[leftEyeIndex[i]][1]))
+def final_ear(shape):
+    (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+    (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
 
-    cv2.fillConvexPoly(mask, np.int32(hullLeftEye), 255)
+    leftEye = shape[lStart:lEnd]
+    rightEye = shape[rStart:rEnd]
 
-    hullRightEye = []
-    for i in range(0, len(rightEyeIndex)):
-        hullRightEye.append((landmarks[rightEyeIndex[i]][0], landmarks[rightEyeIndex[i]][1]))
-
-    cv2.fillConvexPoly(mask, np.int32(hullRightEye), 255)
-
-    leftEAR = eye_aspect_ratio(hullLeftEye)
-    rightEAR = eye_aspect_ratio(hullRightEye)
+    leftEAR = eye_aspect_ratio(leftEye)
+    rightEAR = eye_aspect_ratio(rightEye)
 
     ear = (leftEAR + rightEAR) / 2.0
+    return (ear, leftEye, rightEye)
 
-    eyeStatus = 1  # 1 -> Open, 0 -> Closed
-    if ear < thresh:
-        eyeStatus = 0
+def lip_distance(shape):
+    top_lip = shape[50:53]
+    top_lip = np.concatenate((top_lip, shape[61:64]))
 
-    return eyeStatus
+    low_lip = shape[56:59]
+    low_lip = np.concatenate((low_lip, shape[65:68]))
 
-# Function to check blink status and drowsiness
-def checkBlinkStatus(eyeStatus):
-    global state, blinkCount, drowsy
-    if 0 <= state <= falseBlinkLimit:
-        if eyeStatus:
-            state = 0
-        else:
-            state += 1
-    elif falseBlinkLimit <= state < drowsyLimit:
-        if eyeStatus:
-            blinkCount += 1
-            state = 0
-        else:
-            state += 1
-    else:
-        if eyeStatus:
-            state = 0
-            drowsy = 1
-            blinkCount += 1
-        else:
-            drowsy = 1
+    top_mean = np.mean(top_lip, axis=0)
+    low_mean = np.mean(low_lip, axis=0)
 
-# Function to get facial landmarks
-def getLandmarks(im):
-    imSmall = cv2.resize(im, None, fx=1.0/FACE_DOWNSAMPLE_RATIO, fy=1.0/FACE_DOWNSAMPLE_RATIO, interpolation=cv2.INTER_LINEAR)
-    rects = detector(imSmall, 0)
-    if len(rects) == 0:
-        return 0
-    newRect = dlib.rectangle(int(rects[0].left() * FACE_DOWNSAMPLE_RATIO), int(rects[0].top() * FACE_DOWNSAMPLE_RATIO),
-                            int(rects[0].right() * FACE_DOWNSAMPLE_RATIO), int(rects[0].bottom() * FACE_DOWNSAMPLE_RATIO))
-    points = []
-    [points.append((p.x, p.y)) for p in predictor(im, newRect).parts()]
-    return points
+    distance = abs(top_mean[1] - low_mean[1])
+    return distance
 
-# Capture video from the camera
-capture = cv2.VideoCapture(0)
 
-# Capture a few frames for calibration
-for i in range(10):
-    ret, frame = capture.read()
+ap = argparse.ArgumentParser()
+ap.add_argument("-w", "--webcam", type=int, default=0,
+                help="index of webcam on system")
+args = vars(ap.parse_args())
 
-totalTime = 0.0
-validFrames = 0
-dummyFrames = 100
+EYE_AR_THRESH = 0.3
+EYE_AR_CONSEC_FRAMES = 30
+YAWN_THRESH = 20
+alarm_status = False
+alarm_status2 = False
+saying = False
+COUNTER = 0
 
-print("Calibration in Progress!")
-while validFrames < dummyFrames:
-    validFrames += 1
-    t = time.time()
-    ret, frame = capture.read()
-    height, width = frame.shape[:2]
-    IMAGE_RESIZE = np.float32(height) / RESIZE_HEIGHT
-    frame = cv2.resize(frame, None, fx=1/IMAGE_RESIZE, fy=1/IMAGE_RESIZE, interpolation=cv2.INTER_LINEAR)
-    adjusted = histogram_equalization(frame)
-    landmarks = getLandmarks(adjusted)
-    timeLandmarks = time.time() - t
+print("-> Loading the predictor and detector...")
+#detector = dlib.get_frontal_face_detector()
+detector = cv2.CascadeClassifier("/Users/shrestabanerjee/Desktop/tata/Drowsiness-and-Yawn-Detection-with-voice-alert-using-Dlib-master/haarcascade_frontalface_default.xml")    #Faster but less accurate
+predictor = dlib.shape_predictor('/Users/shrestabanerjee/Desktop/tata/Drowsiness-and-Yawn-Detection-with-voice-alert-using-Dlib-master/shape_predictor_68_face_landmarks.dat')
 
-    if landmarks == 0:
-        validFrames -= 1
-        cv2.putText(frame, "Unable to detect face, Please check proper lighting", (10, 30), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
-        cv2.putText(frame, "or decrease FACE_DOWNSAMPLE_RATIO", (10, 50), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
-        cv2.imshow("Blink Detection Demo", frame)
-        if cv2.waitKey(1) & 0xFF == 27:
-            sys.exit()
-    else:
-        totalTime += timeLandmarks
+
+print("-> Starting Video Stream")
+vs = VideoStream(src=args["webcam"]).start()
+#vs= VideoStream(usePiCamera=True).start()       //For Raspberry Pi
+time.sleep(1.0)
+
+while True:
+
+    frame = vs.read()
+    frame = imutils.resize(frame, width=450)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    #rects = detector(gray, 0)
+    rects = detector.detectMultiScale(gray, scaleFactor=1.1, 
+		minNeighbors=5, minSize=(30, 30),
+		flags=cv2.CASCADE_SCALE_IMAGE)
+
+    #for rect in rects:
+    for (x, y, w, h) in rects:
+        rect = dlib.rectangle(int(x), int(y), int(x + w),int(y + h))
         
-print("Calibration Complete!")
+        shape = predictor(gray, rect)
+        shape = face_utils.shape_to_np(shape)
 
-spf = totalTime / dummyFrames
-print("Current SPF (seconds per frame) is {:.2f} ms".format(spf * 1000))
+        eye = final_ear(shape)
+        ear = eye[0]
+        leftEye = eye [1]
+        rightEye = eye[2]
 
-drowsyLimit = drowsyTime / spf
-falseBlinkLimit = blinkTime / spf
-print("drowsy limit: {}, false blink limit: {}".format(drowsyLimit, falseBlinkLimit))
+        distance = lip_distance(shape)
 
-if __name__ == "__main__":
-    vid_writer = cv2.VideoWriter('output-low-light-2.avi', cv2.VideoWriter_fourcc('M','J','P','G'), 15, (frame.shape[1],frame.shape[0]))
-    while True:
-        try:
-            t = time.time()
-            ret, frame = capture.read()
-            height, width = frame.shape[:2]
-            IMAGE_RESIZE = np.float32(height) / RESIZE_HEIGHT
-            frame = cv2.resize(frame, None, fx=1/IMAGE_RESIZE, fy=1/IMAGE_RESIZE, interpolation=cv2.INTER_LINEAR)
-            adjusted = histogram_equalization(frame)
-            landmarks = getLandmarks(adjusted)
-            
-            if landmarks == 0:
-                validFrames -= 1
-                cv2.putText(frame, "Unable to detect face, Please check proper lighting", (10, 30), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
-                cv2.putText(frame, "or decrease FACE_DOWNSAMPLE_RATIO", (10, 50), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
-                cv2.imshow("Blink Detection Demo", frame)
-                if cv2.waitKey(1) & 0xFF == 27:
-                    break
-                continue
-            
-            eyeStatus = checkEyeStatus(landmarks)
-            checkBlinkStatus(eyeStatus)
+        leftEyeHull = cv2.convexHull(leftEye)
+        rightEyeHull = cv2.convexHull(rightEye)
+        cv2.drawContours(frame, [leftEyeHull], -1, (255, 255, 255), 1)
+        cv2.drawContours(frame, [rightEyeHull], -1, (255, 255, 255), 1)
 
-            for i in range(0, len(leftEyeIndex)):
-                cv2.circle(frame, (landmarks[leftEyeIndex[i]][0], landmarks[leftEyeIndex[i]][1]), 1, (0, 0, 255), -1, lineType=cv2.LINE_AA)
+        lip = shape[48:60]
+        cv2.drawContours(frame, [lip], -1, (255, 255, 255), 1)
 
-            for i in range(0, len(rightEyeIndex)):
-                cv2.circle(frame, (landmarks[rightEyeIndex[i]][0], landmarks[rightEyeIndex[i]][1]), 1, (0, 0, 255), -1, lineType=cv2.LINE_AA)
+        if ear < EYE_AR_THRESH:
+            COUNTER += 1
 
-            if drowsy:
-                cv2.putText(frame, "! ! ! DROWSINESS ALERT ! ! !", (70, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
-                if not ALARM_ON:
-                    ALARM_ON = True
-                    threadStatusQ.put(not ALARM_ON)
-                    thread = Thread(target=soundAlert, args=(sound_path, threadStatusQ,))
-                    thread.setDaemon(True)
-                    thread.start()
-            else:
-                cv2.putText(frame, "Blinks : {}".format(blinkCount), (460, 80), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 0, 255), 2, cv2.LINE_AA)
-                ALARM_ON = False
+            if COUNTER >= EYE_AR_CONSEC_FRAMES:
+                if alarm_status == False:
+                    alarm_status = True
+                    t = Thread(target=alarm, args=('wake up sir',))
+                    t.deamon = True
+                    t.start()
 
-            cv2.imshow("Blink Detection Demo", frame)
-            vid_writer.write(frame)
+                cv2.putText(frame, "DROWSINESS ALERT!", (10, 30),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
-            k = cv2.waitKey(1)
-            if k == ord('r'):
-                state = 0
-                drowsy = 0
-                ALARM_ON = False
-                threadStatusQ.put(not ALARM_ON)
-            elif k == 27:
-                break
+        else:
+            COUNTER = 0
+            alarm_status = False
 
-        except Exception as e:
-            print(e)
+        if (distance > YAWN_THRESH):
+                cv2.putText(frame, "Yawn Alert", (10, 30),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+                if alarm_status2 == False and saying == False:
+                    alarm_status2 = True
+                    t = Thread(target=alarm, args=('take some fresh air sir',))
+                    t.deamon = True
+                    t.start()
+        else:
+            alarm_status2 = False
 
-    capture.release()
-    vid_writer.release()
-    cv2.destroyAllWindows()
+        cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        cv2.putText(frame, "YAWN: {:.2f}".format(distance), (300, 60),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == ord("q"):
+        break
+
+cv2.destroyAllWindows()
+vs.stop()
